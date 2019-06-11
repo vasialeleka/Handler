9ccc2a135e67722c09af28adb9b20350f019bac6
Vasia <vasialeleka@ukr.net>
2019-06-11 08:57:05 +0300

    add style for activity

--- app/src/main/java/com/vasialeleka/handler/MainActivity.kt
+++ app/src/main/java/com/vasialeleka/handler/MainActivity.kt
@@ -2,8 +2,29 @@ package com.vasialeleka.handler
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
+import java.util.concurrent.TimeUnit
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity() , View.OnClickListener{
+    override fun onClick(v: View?) {
+        when(v){
+            btnStart -> startSomething()
+        }
+    }
+
+    private fun startSomething() {
+        for (i in 0..10){
+            // something long
+            longProcess()
+            info.text = "Long proceess" + i
+
+        }
+    }
+
+    private fun longProcess() {
+        TimeUnit.SECONDS.sleep(1)
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
